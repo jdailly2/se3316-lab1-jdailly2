c462cdf26a2c60853099500195b7a0d9c187ded3
jdailly2 <90720926+jdailly2@users.noreply.github.com>
2022-09-16 12:46:23 -0400

    number search
    Created the second search bar so that when you input a number it shows the pokemon that match that number

--- searchBar.js
+++ searchBar.js
@@ -35,3 +35,27 @@ function allLetter(inputtxt)
         document.getElementById('pokemonName').value =  input.slice(0, -1);
      }
   }
+
+  function search_pokemon_number() {
+    let input = document.getElementById('pokemonNumber').value
+   // input=input.toLowerCase();
+    let x = document.getElementsByClassName('pokemon_num');//returns an array of all the pokemons numbers
+    let y = document.getElementsByClassName('pokemon_name')
+    const pokemon_list = [];
+    let counter = 0;
+    for (i = 0; i < x.length; i++) { 
+        console.log(x[i]);
+        console.log("here");
+        
+        if (!x[i].innerHTML.includes(input)) {
+
+        }
+        else {
+            pokemon_list[counter] = y[i].innerHTML;
+            counter++;               
+        }
+    }
+    if(counter>0){
+    alert(pokemon_list);
+    }
+}
