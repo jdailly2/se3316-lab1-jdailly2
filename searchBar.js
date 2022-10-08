c63fbb779dbb293ac2485a6da3f2d430ed977ae9
jdailly2 <90720926+jdailly2@users.noreply.github.com>
2022-10-08 14:08:29 -0400

    working on number search
    able to search using number search

--- searchBar.js
+++ searchBar.js
@@ -2,27 +2,28 @@
 //This function searches through the pokemons names and returns the first 5 matches
 //=================================
 const pokemon_list = [];
-const box=document.getElementById('popUpList')
-const div = document.getElementById('popUp')
+const box=document.getElementById('popUpList');
+const div = document.getElementById('popUp');
 const pokemonName = [];
 var universalCounter = 0;
 let counter = 0;
-var newUl = document.createElement('ul')
+var newUl = document.createElement('ul');
+const PokemonNumbers = [];
 //let div = document.createElement('div')
 function search_pokemon_name(inputtxt) {
   
   //console.log(pokemon_list)
 
-    let input = document.getElementById('pokemonName').value
+    let input = document.getElementById('pokemonName').value;
     input=input.toLowerCase();
     let x = document.getElementsByClassName('pokemon_name');//returns an array of all the pokemons names
-    let des = document.getElementsByClassName('description')
+    let des = document.getElementsByClassName('description');
    // let pic = document.getElementsByClassName('pokemon');
     //let num = document.getElementsByClassName('pokemon_num');
     
 
-    let picture = document.getElementById('poki').getElementsByTagName('img')
-    var img = document.createElement('img')
+    let picture = document.getElementById('poki').getElementsByTagName('img');
+    var img = document.createElement('img');
     
    // const box=document.getElementById('popUpList')
    
@@ -41,7 +42,7 @@ function search_pokemon_name(inputtxt) {
        
      // }
 
-     removeSearch()
+     removeSearch();
      
      
       counter = 0;
@@ -59,7 +60,7 @@ function search_pokemon_name(inputtxt) {
         }
         else {
             pokemon_list[counter] = x[i].innerHTML + "  " + des[i].innerHTML;
-            pokemonName[counter] = x[i].innerHTML
+            pokemonName[counter] = x[i].innerHTML;
             
             allImages[counter] = picture[i].cloneNode(true);
             counter++;                 
@@ -68,17 +69,17 @@ function search_pokemon_name(inputtxt) {
   }
   console.log(allImages)
 for(j=0; j<allImages.length;j++){
-  var li = document.createElement('li')
-  li.className = "child"
-  li.id=pokemonName[j]
-  p = pokemonName[j] 
+  var li = document.createElement('li');
+  li.className = "child";
+  li.id=pokemonName[j];
+  p = pokemonName[j] ;
 
-  img = allImages[j]
+  img = allImages[j];
 
-  box.appendChild(li)
+  box.appendChild(li);
   
-  li.append(p)
-  li.appendChild(img)
+  li.append(p);
+  li.appendChild(img);
   
 
   
@@ -104,11 +105,9 @@ function removeSearch(){
   let children = document.querySelectorAll('.child');
 for(let c of children){
   
-  temp.removeChild(c)
-console.log("I am here at the moment")
+  temp.removeChild(c);
+console.log("I am here at the moment");
 }
-//temp.remove();
-//div.appendChild(newUl)
 }
 
 
@@ -119,7 +118,7 @@ console.log("I am here at the moment")
 function allLetter(inputtxt)
   {
 
-    var input = document.getElementById('pokemonName').value
+    var input = document.getElementById('pokemonName').value;
 
    if(/^[a-zA-Z]+$/.test(input))
      {
@@ -139,7 +138,7 @@ alert(pokemon_list);
 //=================================
 //This function searches through the pokemons numbers and returns the first 5 matches
 //=================================
-  function search_pokemon_number() {
+  function search_pokemon_number(inputtxt) {
     let input = document.getElementById('pokemonNumber').value
   
     let x = document.getElementsByClassName('pokemon_num');//returns an array of all the pokemons numbers
@@ -147,20 +146,47 @@ alert(pokemon_list);
     let des = document.getElementsByClassName('description');
     const pokemon_list = [];
     let counter = 0;
+    let picture = document.getElementById('poki').getElementsByTagName('img');
+    var img = document.createElement('img');
+    let p = document.createElement('p');
+    const allImages = [];
+   
+   
+    removeSearch()
+  
+
+counter = 0;
+    if(!allNumbers(inputtxt)){
+    
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.includes(input)) {
+          
+      
         }
         else {
             pokemon_list[counter] = y[i].innerHTML + "  " + des[i].innerHTML;
-            counter++;
-            if(counter >4){
-                break;
-            }               
+            PokemonNumbers[counter] = x[i].innerHTML;
+            allImages[counter] = picture[i].cloneNode(true);
+            counter++;      
+         
         }
+       
     }
-    if(counter>0){
-    alert(pokemon_list);
-    }
+    for(j=0; j<allImages.length;j++){
+   
+      var li = document.createElement('li');
+      li.className = "child";
+      li.id=PokemonNumbers[j];
+      p = PokemonNumbers[j] ;
+      img = allImages[j];
+    
+      box.appendChild(li);
+      
+      li.append(p);
+      li.appendChild(img);
+ 
+}
+  }
 }
 //=================================
 //This function takes input from the user and make sure that the user does
